447bdf456c350549c195b775c7a977c2571d90bc
selamtesfaye2x-cmd <selamtesfaye2x@gmail.com>
2026-08-21 13:38:34 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,10 @@
 const SUPABASE_URL = "https://zngnhssgpqxvimfocpil.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_zbiPWLKaYRxQU-R3IgAO-A_RfkGfBZH";
 
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+const SUPABASE_KEY =
+  "sb_publishable_zbiPWLKaYRxQU-R3IgAO-A_RfkGfBZH";
+
+const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
