8c6aa3a10f96201c0580aad09d41dda7349d7550
selamtesfaye2x-cmd <selamtesfaye2x@gmail.com>
2026-08-21 14:55:52 +0300

    Update and rename supabase.js to Supabase.js

--- Supabase.js
+++ Supabase.js
@@ -1,10 +1,9 @@
 const SUPABASE_URL = "https://zngnhssgpqxvimfocpil.supabase.co";
 
 const SUPABASE_ANON_KEY =
-"sb_publishable_zbiPWLKaYRxQU-R3IgAO-A_RfkGfBZH";
+  "sb_publishable_zbiPWLKaYRxQU-R3IgAO-A_RfkGfBZH";
 
-const supabaseClient =
-window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
